e76b97d4ac92348bbf6582a997404fe292743f23
Tim Hood <tim.z.hood@gmail.com>
2023-04-23 18:28:29 +0100

    Enabled and used clang-tidy.

--- exercise-01-pointers/pointers.cpp
+++ exercise-01-pointers/pointers.cpp
@@ -8,11 +8,13 @@
 using std::cout;
 using std::endl;
 
-void SwapPointers(int *x, int *y)
+void SwapPointers(int *x_number, int *y_number)
 {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+    // auto timpy = *x;
+
+    const int temp = *x_number;
+    *x_number = *y_number;
+    *y_number = temp;
 }
 
 // void swap_references(int &x, int &y)
@@ -22,13 +24,13 @@ void SwapPointers(int *x, int *y)
 //     y = temp;
 // }
 
-int main()
+auto main() -> int
 {
     cout << "hello world" << endl;
     printf("hello world \n");
 
     // pointer to an "int", currently unassigned, actually now assign to "null"
-    int *ip = nullptr;
+    int *int_ptr = nullptr;
 
     // declare "int" variable "total" and initialise to zero
     int total = 0;
@@ -36,15 +38,16 @@ int main()
     cout << "total=" << total << endl;
 
     // use "address-of" operator, assign the address of "total" to "ip", now "ip" points to "total"
-    ip = &total;
+    int_ptr = &total;
 
     // use "dereference" operator, get what "ip" points at, i.e.: the value of "total"
-    printf("*ip=%d \n", *ip);
-    cout << "*ip=" << *ip << endl;
+    printf("*int_ptr=%d \n", *int_ptr);
+    cout << "*int_ptr=" << *int_ptr << endl;
 
     // change "total" directly and see what "ip" points at
-    total = 123;
-    cout << "*ip=" << *ip << endl;
+    const int INITIAL_VALUE = 123;
+    total = INITIAL_VALUE;
+    cout << "*int_ptr=" << *int_ptr << endl;
 
     // swap some things
     int count = 0;
@@ -58,10 +61,12 @@ int main()
     // cout << "total=" << total << endl;
 
     cout << "before increment via pointer, total=" << total << endl;
-    (*ip)++;
+    (*int_ptr)++;
     cout << "after increment via pointer, total=" << total << endl;
-    (*ip)--;
+    (*int_ptr)--;
     cout << "after decrement via pointer, total=" << total << endl;
 
     cout << "goodbye world" << endl;
+
+    return 0;
 }
